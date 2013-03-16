9ad76e030ab4a66ca4ce00184c43286093406f6b
Chris Jenkins <chris.mark.jenkins@gmail.com>
2013-03-16 12:12:32 +0000

    modified the demo to make it stop moving before it lands

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ var express = require('express');
 var client = arDrone.createClient();
 var app = express();
 
-app.get('/', function(req, res){
-	  var page = fs.readFileSync('view/index.html');
-	  res.setHeader('Content-Type', 'text/html');
-	  res.setHeader('Content-Length', page.length);
-	  res.end(page);
-	});
+app.get('/', function (req, res) {
+  var page = fs.readFileSync('view/index.html');
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', page.length);
+  res.end(page);
+});
 
-app.get('/demo', function(req, res){
-	  var page = fs.readFileSync('view/demo.js');
-	  res.setHeader('Content-Type', 'text/javascript');
-	  res.setHeader('Content-Length', page.length);
-	  res.end(page);
-    demo();
-	});
+app.get('/demo', function (req, res) {
+  var page = fs.readFileSync('view/demo.js');
+  res.setHeader('Content-Type', 'text/javascript');
+  res.setHeader('Content-Length', page.length);
+  res.end(page);
+  demo();
+});
 png(client, { port: 8000 });
 app.listen(3000);
 
@@ -29,28 +29,30 @@ console.log('Web server listening on port 3000');
 
 //end web server stuff
 
-function demo(){
+function demo() {
   client.takeoff();
 
   client
-    .after(5000, function() {
-      this.clockwise(0.25);
-    })
-    .after(5000, function() {
-      this.clockwise(0.25);
-    })
-    .after(5000, function() {
-      this.clockwise(0.25);
-    })
-    .after(5000, function() {
-      this.clockwise(0.25);
-    })
-    .after(3000, function() {
-      this.animate('flipLeft', 15);
-    })
-    .after(1000, function() {
-      this.land();
-      this.stop();
-    });
+      .after(5000, function () {
+        this.clockwise(0.25);
+      })
+      .after(5000, function () {
+        this.clockwise(0.25);
+      })
+      .after(5000, function () {
+        this.clockwise(0.25);
+      })
+      .after(5000, function () {
+        this.clockwise(0.25);
+      })
+      .after(3000, function () {
+        this.animate('flipLeft', 15);
+      })
+      .after(1000, function () {
+        this.stop();
+      })
+      .after(2000, function () {
+        this.land();
+      });
 }
   
