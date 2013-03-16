cbb01cdfc7f43738b62dd74b32142a15e1f17e6b
Joe Warren <joe.warren@holidayextras.com>
2013-03-16 11:54:34 +0000

    Added demo function

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var arDrone = require('ar-drone');
 var png = require('ar-drone-png-stream');
 
-#web server stuff
+//web server stuff
 var fs = require('fs');
 var express = require('express');
 var app = express();
@@ -18,36 +18,39 @@ app.get('/demo', function(req, res){
 	  res.setHeader('Content-Type', 'text/javascript');
 	  res.setHeader('Content-Length', page.length);
 	  res.end(page);
+    demo();
 	});
 
 app.listen(3000);
 console.log('Web server listening on port 3000');
 
-#end web server stuff
+//end web server stuff
 
 var client = arDrone.createClient();
 png(client, { port: 8000 });
 
-client.takeoff();
+function demo(){
+  client.takeoff();
 
-client
-  .after(5000, function() {
-    this.clockwise(0.25);
-  })
-  .after(5000, function() {
-    this.clockwise(0.25);
-  })
-  .after(5000, function() {
-    this.clockwise(0.25);
-  })
-  .after(5000, function() {
-    this.clockwise(0.25);
-  })
-  .after(3000, function() {
-    this.animate('flipLeft', 15);
-  })
-  .after(1000, function() {
-    this.land();
-    this.stop();
-  });
+  client
+    .after(5000, function() {
+      this.clockwise(0.25);
+    })
+    .after(5000, function() {
+      this.clockwise(0.25);
+    })
+    .after(5000, function() {
+      this.clockwise(0.25);
+    })
+    .after(5000, function() {
+      this.clockwise(0.25);
+    })
+    .after(3000, function() {
+      this.animate('flipLeft', 15);
+    })
+    .after(1000, function() {
+      this.land();
+      this.stop();
+    });
+}
   
