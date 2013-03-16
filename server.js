646e168fc2d191a233f23021f858427026cdd775
hx-markterry <mark.terry@holidayextras.com>
2013-03-16 11:43:39 +0000

    added web server code

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 var arDrone = require('ar-drone');
 var png = require('ar-drone-png-stream');
 
+#web server stuff
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	  var page = fs.readFileSync('view/index.html');
+	  res.setHeader('Content-Type', 'text/html');
+	  res.setHeader('Content-Length', page.length);
+	  res.end(page);
+	});
+
+app.get('/demo', function(req, res){
+	  var page = fs.readFileSync('view/demo.js');
+	  res.setHeader('Content-Type', 'text/javascript');
+	  res.setHeader('Content-Length', page.length);
+	  res.end(page);
+	});
+
+app.listen(3000);
+console.log('Web server listening on port 3000');
+
+#end web server stuff
+
 var client = arDrone.createClient();
 png(client, { port: 8000 });
 
