29e05948ce60e3c72438827ab5b3b3d74c73d1cc
Chris Jenkins <chris.mark.jenkins@gmail.com>
2013-03-16 12:36:29 +0000

    added animate leds

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ var express = require('express');
 var client = arDrone.createClient();
 var app = express();
 
-app.get('/', function(req, res){
-      renderPage(res, 'view/index.html', 'text/html');
-	});
+app.get('/', function (req, res) {
+  renderPage(res, 'view/index.html', 'text/html');
+});
 
-app.get('/assets/js/main.js', function(req, res){
-      renderPage(res, 'assets/js/main.js', 'text/javascript');
-	});
+app.get('/assets/js/main.js', function (req, res) {
+  renderPage(res, 'assets/js/main.js', 'text/javascript');
+});
 
-app.get('/assets/js/jquery.min.js', function(req, res){
-      renderPage(res, 'assets/js/jquery.min.js', 'text/javascript');
-	});
+app.get('/assets/js/jquery.min.js', function (req, res) {
+  renderPage(res, 'assets/js/jquery.min.js', 'text/javascript');
+});
 
-app.get('/demo', function(req, res){
-      renderPage(res, 'view/demo.js', 'text/javascript');
-      demo();
-	});
+app.get('/demo', function (req, res) {
+  renderPage(res, 'view/demo.js', 'text/javascript');
+  demo();
+});
 
 png(client, { port: 8000 });
 console.log('PNG server listening on port 8000');
@@ -32,8 +32,8 @@ console.log('Web server listening on port 3000');
 //end web server stuff
 
 function demo() {
+  client.animateLeds("snakeGreenRed", 1, 10);
   client.takeoff();
-
   client
       .after(5000, function () {
         this.clockwise(0.25);
@@ -59,9 +59,9 @@ function demo() {
 }
 
 //display a page to the browser
-function renderPage(res, file, type){
+function renderPage(res, file, type) {
   var page = fs.readFileSync(file);
   res.setHeader('Content-Type', type);
   res.setHeader('Content-Length', page.length);
-  res.end(page);	
+  res.end(page);
 }
